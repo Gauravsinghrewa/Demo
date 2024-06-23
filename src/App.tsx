9fcc71fe816c34c9c39cda4ef59cee12c5533e14
Gauravsingh <singhgaurav.rewamp@gmail.com>
2024-06-23 08:56:00 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/button'
+import './App.css'
+import { Calendar } from "./components/ui/calendar"
+
+function App() {
+
+  return (
+    <>
+       <Button>Click me</Button>
+       <Calendar/>
+    </>
+  )
+}
+
+export default App
